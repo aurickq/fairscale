adding a unit test to check the gradient reducing, seems correct

fairscale/nn/data_parallel/sharded_ddp.py
@@ -126,12 +126,13 @@ class ShardedDataParallel(nn.Module):
                 raise RuntimeError("OssDdp requires explicit reduction, must call OssDdp.reduce")
             if not self.accumulate_grads:
                 self.need_reduction = True
-            if self.broadcast_buffers:
-                self._sync_buffers()
+
+        if self.broadcast_buffers:
+            self._sync_buffers()
 
         return self.module(*inputs, **kwargs)
 
-    def reduce(self) -> None:
+    def reduce(self, free_temporary_grads: bool = True) -> None:
         """
         This function must be called explicitly after backward to reduce
         gradients. There is no automatic hook like c10d.
@@ -150,32 +151,40 @@ class ShardedDataParallel(nn.Module):
                 group=self.process_group,
                 self_rank=self.rank,
                 world_size=self.world_size,
+                free_temporary_grads=free_temporary_grads,
             )
 
     @staticmethod
     def _reduce_grads_task(
-        buffers: List[torch.Tensor], per_rank_params: List[List[Parameter]], group: Any, self_rank: int, world_size: int
+        buffers: List[torch.Tensor],
+        per_rank_params: List[List[Parameter]],
+        group: Any,
+        self_rank: int,
+        world_size: int,
+        free_temporary_grads: bool,
     ) -> None:
         """Helper to reduce a list of params. The params are sorted by size, smallest first, which allows for
         an opportunistic bucketing.
 
         .. warning: All param gradients are assumed to exist
-        .. warning: Reduced grads are removed from the ranks which don't own them, to save memory """
+        .. warning: Reduced grads are removed from the ranks which don't own them, to save memory"""
 
         buffer_size = buffers[0].numel()
         bucket_requests = []
-        requests = []
+        direct_requests: List[Any] = []
 
-        for (rank, params), buffer in zip(enumerate(per_rank_params), buffers):
+        # First issue all the reduce requests, for all devices, and collect the pseudo-futures. Two parts:
+        #  - the smallest gradients are bucketed
+        #  - the biggest are reduced directly
+        for (dst_rank, params), buffer in zip(enumerate(per_rank_params), buffers):
             # All the params are sorted per rank and per increasing size
             if len(params) == 0:
                 continue
 
-            for p in params:
-                if p.grad is None:
-                    p.grad = torch.zeros_like(p)
+            for p in filter(lambda x: x.grad is None, params):
+                p.grad = torch.zeros_like(p)
 
-            global_rank = OSS.get_global_rank(group, rank)
+            global_rank = OSS.get_global_rank(group, dst_rank)
 
             # Copy small gradients into per-GPU buffers and then async reduce
             i_bucketed = 0  # the number of tensors packed in the buffer
@@ -187,7 +196,7 @@ class ShardedDataParallel(nn.Module):
                 buffer[offset:end].copy_(params[i_bucketed].grad.data.view(-1))  # type: ignore
                 offset = end
 
-                if rank != self_rank:
+                if free_temporary_grads and dst_rank != self_rank:
                     # This rank is not the owner, these gradients have been reduced and can be released
                     params[i_bucketed].grad = None
 
@@ -198,7 +207,7 @@ class ShardedDataParallel(nn.Module):
                 bucket_requests.append(
                     (
                         dist.reduce(tensor=buffer, dst=global_rank, group=group, async_op=True),  # type: ignore
-                        rank,
+                        dst_rank,
                     )
                 )
 
@@ -209,18 +218,18 @@ class ShardedDataParallel(nn.Module):
                     raise RuntimeError("DistributedDataParallel only works with gradients that don't require grad")
 
                 p.grad.div_(world_size)  # type: ignore
-                requests.append((dist.reduce(tensor=p.grad, dst=global_rank, group=group, async_op=True), rank, p))  # type: ignore
+                direct_requests.append((dist.reduce(tensor=p.grad, dst=global_rank, group=group, async_op=True), dst_rank, p))  # type: ignore
 
-        # Unroll the initial packed small gradients, as soon as possible
-        for future, rank in bucket_requests:
-            future.wait()
+        # Now unroll the initial packed small gradients, as soon as possible
+        for work_handle, dst_rank in bucket_requests:
+            work_handle.wait()
 
-            if rank == self_rank:
+            if dst_rank == self_rank:
                 # This rank is the owner, unpack the results in the appropriate parameters
                 i_bucketed = 0  # the number of tensors packed in the buffer
                 offset = 0
-                params = per_rank_params[rank]
-                buffer = buffers[rank]
+                params = per_rank_params[dst_rank]
+                buffer = buffers[dst_rank]
 
                 while i_bucketed < len(params) and offset + params[i_bucketed].numel() < buffer_size:
                     end = offset + params[i_bucketed].numel()
@@ -228,10 +237,10 @@ class ShardedDataParallel(nn.Module):
                     offset = end
                     i_bucketed += 1
 
-        # Make sure that we're done with this device before moving on and cleaning the unused params
-        for future, rank, param in requests:
-            future.wait()
-            if rank != self_rank:
+        # Finally, make sure that we're done with this device before moving on and cleaning the unused params
+        for work_handle, dst_rank, param in direct_requests:
+            work_handle.wait()
+            if free_temporary_grads and dst_rank != self_rank:
                 # This gradient has been reduced and this rank is not the owner, it can be released
                 param.grad = None
 

tests/nn/data_parallel/test_sharded_ddp.py
@@ -23,7 +23,7 @@ skip_if_single_gpu = pytest.mark.skipif(torch.cuda.device_count() < 2, reason="m
 
 
 def test_on_cpu():
-    run_test(backend=dist.Backend.GLOO, device=torch.device("cpu"), world_size=4)
+    run_test(backend=dist.Backend.GLOO, device=torch.device("cpu"), world_size=10)
 
 
 @skip_if_no_cuda
@@ -42,7 +42,7 @@ def run_one_step(rank, world_size, backend, device, temp_file_name):
     np.random.seed(rank)
 
     # Any model works. Add one different buffer per rank
-    model = Sequential(Linear(2, 3), Linear(3, 4)).to(device)
+    model = Sequential(Linear(2, 3), Linear(3, 3), Linear(3, 3), Linear(3, 3), Linear(3, 3)).to(device)
     model.register_buffer("test_buffer", torch.ones((1)) * rank)
     model.to(device)
 
@@ -61,6 +61,7 @@ def run_one_step(rank, world_size, backend, device, temp_file_name):
         if dist.get_backend() != "nccl":
             for pg in optimizer.param_groups:
                 for p in pg["params"]:
+                    # Check the params
                     receptacle = [p.clone() for _ in range(world_size)] if rank == 0 else []
                     dist.gather(p, receptacle, dst=0)
                     if rank == 0:
@@ -77,22 +78,44 @@ def run_one_step(rank, world_size, backend, device, temp_file_name):
     # Optim loop
     def closure():
         optimizer.zero_grad()
-        # Different input per rank, allows for checking that the gradients have been properly reduced
-        input_tensor = torch.zeros((64, 2)).to(device) if rank == 0 else torch.ones((64, 2)).to(device)
 
+        input_tensor = torch.ones((64, 2)).to(device)
         loss = ddp(input_tensor).abs().sum()
         loss.backward()
 
+        # Nuke all the grads for the first rank, to check the reduction
         if rank == 0:
-            assert optimizer.optim.param_groups[0]["params"][0].grad.sum().item() == 0.0
+            for pg in optimizer.param_groups:
+                for p in pg["params"]:
+                    p.grad.fill_(0.0)
 
-        ddp.reduce()
+        ddp.reduce(free_temporary_grads=False)  # Keep all grads up to check the reduction
+
+        # Check that the grads have been properly reduced
+        if dist.get_backend() != "nccl":
+            for pg in optimizer.param_groups:
+                for p in pg["params"]:
+                    owner = optimizer.param_to_rank[p]
+                    is_owner = rank == owner
+
+                    # The grad was initially 0 on rank 0, and something for all other ranks
+                    # Check that after reduction, the owner has the correct value
+                    receptacle = [p.grad.clone() for _ in range(world_size)] if is_owner else []
+                    dist.gather(p.grad, receptacle, dst=owner)
+
+                    if is_owner:
+                        # The recipient grad have been modified in place, so the current value cannot be an input
+                        reference = rank + 1 if rank < world_size - 1 else 1
+                        ref_value = receptacle[reference] * (world_size - 1) / world_size
+                        valid = torch.all(torch.isclose(p.grad, ref_value))
+                        assert valid, "Rank {}: Gradient reduction failed : \n{} \nvs \n{} \n ** \n{}".format(
+                            rank, p.grad, ref_value, torch.isclose(p.grad, ref_value)
+                        )
 
         return loss
 
-    # The raw gradients are different in between the ranks, check that they are properly reduced
-    # the models should stay the same in between the ranks
-    for i in range(10):
+    # The models should stay the same in between the ranks
+    for i in range(5):
         _ = optimizer.step(closure=closure)
         check_same_model_params()
 